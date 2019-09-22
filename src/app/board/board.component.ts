d8a80153ee7b066e7297903666b27d3878ee4fb4
Rupank Bansal <rupank@playpowerlabs.com>
2019-09-22 18:15:08 +0530

    Added TicTacToe Working Code

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: string[];
+  isBabyNext: boolean;
+  winner: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.isBabyNext = true;
+  }
+
+  get player() {
+    return this.isBabyNext ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx] && !this.winner) {
+      this.squares.splice(idx, 1, this.player);
+      this.isBabyNext = !this.isBabyNext;
+    }
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (const line of lines) {
+      const [a, b, c] = line;
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+
+}
